83e4d5e35a177739058aac3d5fe28728d3a0a106
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2022-07-19 14:35:59 -0300

    Order by List

---
+++ app/src/main/java/com/digruttola/kotlin/OrdenarListas.kt
@@ -0,0 +1,31 @@
+package com.example.kotlin
+
+fun main(args: Array<String>) {
+    val numeroLoteria = listOf(11,22,43,56,78,66)
+
+    //orden ascendente
+    val numerosSorted = numeroLoteria.sorted()
+    println("Ascendente: $numerosSorted")
+
+    //orden Desendiente
+    val numerosSortedDesen = numeroLoteria.sortedDescending()
+    println("Desendiente: $numerosSortedDesen")
+
+    //Ascendente por condicion
+    val ordenarPorMultiplos = numeroLoteria.sortedBy { numero -> numero < 50 }
+    println(ordenarPorMultiplos)
+
+    //ordena aleatoriamente
+    val numeroAleatorios = numeroLoteria.shuffled()
+    println(numeroAleatorios)
+
+    //imprime al reves
+    val numeroEnReversa = numeroLoteria.reversed()
+    println(numeroEnReversa)
+
+    val mensajesDeNumeros = numeroLoteria.map { numero -> "Tu numero de Loteria es: $numero" }
+    println(mensajesDeNumeros)
+
+    val numerosFiltrados = numeroLoteria.filter { numero -> numero > 50 }
+    println(numerosFiltrados)
+}
